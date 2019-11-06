d7669f77ac8efeb5274aea05fb4d44d7431d8d0f
Ezio2220 <fjosue447@gmail.com>
2019-11-06 14:22:19 -0600

    Update funciones.js

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -593,7 +593,7 @@ db.once("value",function(snap){
 //----------------------------------------------------------------------------SESIONES--------------------------------------
 const clave = window.localStorage;
 //"D:/works/0/2019/TSI/ProyectoRAF/";
-const base = "D:/works/0/2019/TSI/ProyectoRAF/";//"https://ezio2220.github.io/ProyectoRAF/";
+const base = "https://ezio2220.github.io/ProyectoRAF/";
 
 function salir(){
   console.log(clave.getItem('active'));
